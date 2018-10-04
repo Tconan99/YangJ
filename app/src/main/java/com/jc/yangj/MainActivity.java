9d37ff5f159ebb52feb4f8cae87ecf3e3177c4be
tconan99 <627594232@qq.com>
2018-10-04 20:27:15 +0800

    # 1.1.0 添加防止抢自己红包的功能
    后续计划添加，防止自己红包连续抢代码判断

--- app/src/main/java/com/jc/yangj/MainActivity.java
+++ app/src/main/java/com/jc/yangj/MainActivity.java
@@ -1,13 +1,5 @@
 package com.jc.yangj;
 
-import im.fir.sdk.FIR;
-import im.fir.sdk.VersionCheckCallback;
-
-import java.io.File;
-import java.io.FileInputStream;
-
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.KeyguardManager;
 import android.app.KeyguardManager.KeyguardLock;
@@ -28,176 +20,186 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
 
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import im.fir.sdk.FIR;
+import im.fir.sdk.VersionCheckCallback;
+
 @SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
 
-	private KeyguardLock keyguardLock;
-	private final String LOCK_TAG = "Charge";
+    private KeyguardLock keyguardLock;
+    private final String LOCK_TAG = "Charge";
 
-	private Switch isServiceSwitch = null;
-	private Switch isHaveNoPersonSwitch = null;
-	private Switch isNeedNotifySwitch = null;
+    private Switch isServiceSwitch = null;
 
-	private static MainActivity mainActivity;
-	private static boolean isHand = true;
+    private static boolean isHand = true;
 
-	private static MediaPlayer mPlayer = null;
-	private static Vibrator mVibrator = null;
+    private static MediaPlayer mPlayer = null;
+    private static Vibrator mVibrator = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		mainActivity = this;
+        setContentView(R.layout.activity_main);
 
-		setContentView(R.layout.activity_main);
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        keyguardLock = keyguardManager.newKeyguardLock(LOCK_TAG);
+        keyguardLock.disableKeyguard();
 
-		KeyguardManager keyguardManager = (KeyguardManager)getSystemService(KEYGUARD_SERVICE);
-		keyguardLock = keyguardManager.newKeyguardLock(LOCK_TAG);
-		keyguardLock.disableKeyguard();
+        //启动抢红包服务
+        isServiceSwitch = findViewById(R.id.main_isserviceopen_switch);
+        isServiceSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-		//启动抢红包服务
-		isServiceSwitch = (Switch)findViewById(R.id.main_isserviceopen_switch);
-		isServiceSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isHand) {
+                    Intent killIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                    startActivity(killIntent);
+                }
+                isHand = true;
+            }
+        });
 
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isHand) {
-					Intent killIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-					startActivity(killIntent);
-				}
-				isHand = true;
-			}
-		});
+        // 无人模式
+        Switch isHaveNoPersonSwitch = findViewById(R.id.main_havenoperson_switch);
+        isHaveNoPersonSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-		// 无人模式
-		isHaveNoPersonSwitch = (Switch)findViewById(R.id.main_havenoperson_switch);
-		isHaveNoPersonSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Const.isHaveNoPerson = isChecked;
+            }
+        });
 
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				Const.isHaveNoPerson = isChecked;
-			}
-		});
+        // 是否提醒
+        Switch isNeedNotifySwitch = findViewById(R.id.main_neednotify_switch);
+        isNeedNotifySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Const.isNeedNotify = isChecked;
+            }
+        });
 
-		isNeedNotifySwitch = (Switch)findViewById(R.id.main_neednotify_switch);
-		isNeedNotifySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				Const.isNeedNotify = isChecked;
-			}
-		});
+        // 是否抢自己的红包
+        Switch isNeedOpenSelfSwitch = findViewById(R.id.main_need_open_self_switch);
+        isNeedOpenSelfSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Const.isNeedOpenSelf = isChecked;
+            }
+        });
 
-		//更新按钮
-		findViewById(R.id.main_update_btn).setOnClickListener(new OnClickListener() {
+        //更新按钮
+        findViewById(R.id.main_update_btn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://fir.im/YangJ"));
+                startActivity(intent);
+            }
+        });
 
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(Intent.ACTION_VIEW);
-				intent.setData(Uri.parse("http://fir.im/YangJ"));
-				startActivity(intent);
-			}
-		});
+        mVibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        mPlayer = MediaPlayer.create(this, R.raw.hongbao_arrived);
+        mPlayer.setLooping(false);
 
-		mVibrator = (Vibrator)this.getSystemService(Context.VIBRATOR_SERVICE);
-		mPlayer = MediaPlayer.create(this, R.raw.hongbao_arrived);
-		mPlayer.setLooping(false);
+        findViewById(R.id.main_tips_btn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSound();
+            }
+        });
+    }
 
-		findViewById(R.id.main_tips_btn).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				playSound();
-			}
-		});
-	}
+    public static void playSound() {
 
-	public static void playSound() {
+        try {
 
-		try {
+            if (!Const.isNeedNotify) {
+                return;
+            }
 
-			if (!Const.isNeedNotify) {
-				return;
-			}
+            // 调用震动
+            if (mVibrator != null) {
+                long[] pattern = {100, 400, 100, 400};   // 停止 开启 停止 开启
+                // mVibrator.vibrate(pattern, 2);           //重复两次上面的pattern 如果只想震动一次，index设为-1
+            }
 
-			// 调用震动
-			if (mVibrator!=null) {
-				long[] pattern = {100, 400, 100, 400};   // 停止 开启 停止 开启
-				//mVibrator.vibrate(pattern, 2);           //重复两次上面的pattern 如果只想震动一次，index设为-1
-			}
+            // 播放媒体音乐
+            if (mPlayer != null) {
+                mPlayer.start();
+            }
 
-			// 播放媒体音乐
-			if (mPlayer!=null) {
-				mPlayer.start();
-			}
+        } catch (Exception e) {
+            Log.e("playSound", "提示错误", e);
+        }
 
-		} catch (Exception e) {
-			Log.e("playSound", "提示错误", e);
-		}
+    }
 
-	}
+    private static Thread writePCMThread = null;
+    private static File audioFile = null;
+    private static FileInputStream fileInputStream = null;
+    private static byte buffer[] = new byte[16 * 10000];
 
-	private static AudioTrack audioTrack = null;
-	private static Thread writePCMThread = null;
-	private static File audioFile = null;
-	private static FileInputStream fileInputStream = null;
-	private static byte buffer[] = new byte[16*10000];
+    private static void initAudioTrack() {
+        int minBufferSize = AudioTrack.getMinBufferSize(0xac44, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        System.out.println("minBufferSize = " + minBufferSize);
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 0xac44,
+                AudioFormat.CHANNEL_CONFIGURATION_STEREO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize * 2, AudioTrack.MODE_STREAM);
+        audioTrack.setStereoVolume(1.0f, 1.0f);// 设置当前音量大小
+        System.out.println("initAudioTrack over");
+        audioTrack.play();
+    }
 
-	private static void initAudioTrack()
-	{
-		int minBufferSize = AudioTrack.getMinBufferSize(0xac44, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
-		System.out.println("minBufferSize = "+minBufferSize);
-		audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 0xac44,
-				AudioFormat.CHANNEL_CONFIGURATION_STEREO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize*2,AudioTrack.MODE_STREAM);
-		audioTrack.setStereoVolume(1.0f, 1.0f);// 设置当前音量大小 
-		System.out.println("initAudioTrack over");
-		audioTrack.play();
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isHand = false;
+        isServiceSwitch.setChecked(isOpen());
+        isHand = true;
+        findViewById(R.id.main_update_btn).setEnabled(false);
+        FIR.checkForUpdateInFIR("33f15f26bd0a1bf5693c34ef5c70cb44", new VersionCheckCallback() {
+            @Override
+            public void onSuccess(String versionJson) {
+                Log.i("fir", "check from fir.im success! " + "\n" + versionJson);
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		isHand = false;
-		isServiceSwitch.setChecked(isOpen());
-		isHand = true;
-		findViewById(R.id.main_update_btn).setEnabled(false);
-		FIR.checkForUpdateInFIR("33f15f26bd0a1bf5693c34ef5c70cb44" , new VersionCheckCallback() {
-			@Override
-			public void onSuccess(String versionJson) {
-				Log.i("fir","check from fir.im success! " + "\n" + versionJson);
+                if (true) {
+                    //AlertDialog.Builder builder = new Builder(MainActivity.this);
+                    try {
+                        //远程版本号
+                        int remoteVersionCode = 3;
+                        JSONObject object = new JSONObject(versionJson);
+                        remoteVersionCode = Integer.valueOf(object.get("version").toString());
 
-				if (true) {
-					//AlertDialog.Builder builder = new Builder(MainActivity.this);
-					try {
-						//远程版本号
-						int remoteVersionCode = 3;
-						JSONObject object = new JSONObject(versionJson);
-						remoteVersionCode = Integer.valueOf(object.get("version").toString());
+                        // 本地版本号
+                        int versionCode = MainActivity.this.getPackageManager().getPackageInfo(MainActivity.this.getPackageName(), 0).versionCode;
+                        findViewById(R.id.main_update_btn).setEnabled(versionCode < remoteVersionCode);
 
-						// 本地版本号
-						int versionCode = MainActivity.this.getPackageManager().getPackageInfo(MainActivity.this.getPackageName(), 0).versionCode;
-						findViewById(R.id.main_update_btn).setEnabled(versionCode<remoteVersionCode);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+                }
+            }
 
-				}
-			}
+        });
+    }
 
-		});
-	}
+    // 判断辅助功能是否启动
+    private boolean isOpen() {
+        return Const.isOpen;
+    }
 
-	// 判断辅助功能是否启动
-	private boolean isOpen() {
-		return Const.isOpen;
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (keyguardLock!=null) {
-			keyguardLock.reenableKeyguard();
-		}
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (keyguardLock != null) {
+            keyguardLock.reenableKeyguard();
+        }
+    }
 
 }
